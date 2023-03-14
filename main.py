24f9edaaaa60a9670370ee6d509a8dc11ca26222
abdulhameedshaheen <62672461+abdulhameedshaheen@users.noreply.github.com>
2023-03-14 08:28:05 +0300

    Add Resources and images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -15,3 +16,24 @@ with col2:
     this is my portfiolio website
     """
     st.info(content)
+
+content2 = """
+
+       this is my portfiolio website 
+       this is my portfiolio website
+       this is my portfiolio website
+"""
+st.write(content2)
+
+df=pandas.read_csv("data.csv", sep=";")
+
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
