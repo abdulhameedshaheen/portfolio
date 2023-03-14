7c49aaac22fb65a8d1cd62ffb34a994b0ec3a044
abdulhameedshaheen <62672461+abdulhameedshaheen@users.noreply.github.com>
2023-03-14 08:40:32 +0300

    Add Resources and images

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/1.png")
+    st.image("images/photo.png")
 
 with col2:
     st.title("Abdulhameed Shaheen")
@@ -27,13 +27,14 @@ st.write(content2)
 
 df = pandas.read_csv("data.csv", sep=";")
 
-col3, col4 = st.columns(2)
+col3, empty_col1 , col4 = st.columns([1.5, 0.5, 1.5])
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 
 with col4:
@@ -41,3 +42,4 @@ with col4:
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
