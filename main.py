e0cb61572943e179ef039cd13ac7117b8fa9f6a5
abdulhameedshaheen <62672461+abdulhameedshaheen@users.noreply.github.com>
2023-03-15 08:25:01 +0300

    Add Pages

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 import streamlit as st
 import pandas
 
-#st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("portfolio/images/photo.png")
 
 with col2:
-    st.title("Abdulhameed Shaheen")
+    st.title("Abdulhamid Sheen")
     content = """
-    this is my portfiolio website 
-    this is my portfiolio website
-    this is my portfiolio website
+    this is my portfolio website 
+    this is my portfolio website
+    this is my portfolio website
     """
     st.info(content)
 
 content2 = """
 
-       this is my portfiolio website 
-       this is my portfiolio website
-       this is my portfiolio website
+       this is my portfolio website 
+       this is my portfolio website
+       this is my portfolio website
 """
 st.write(content2)
 
-df = pandas.read_csv("data.csv", sep=";")
+df = pandas.read_csv("portfolio/data.csv", sep=";")
 
-col3, empty_col1 , col4 = st.columns([1.5, 0.5, 1.5])
+col3, empty_col1, col4 = st.columns([1.5, 0.5, 1.5])
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
         st.write(row["description"])
-        st.image("images/" + row["image"])
+        st.image("portfolio/images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
 
 
@@ -41,5 +41,5 @@ with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
-        st.image("images/" + row["image"])
+        st.image("portfolio/images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
